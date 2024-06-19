1f7fed719b7e13ad8fc269823266b82170b52369
Mathewix <halama12@stud.uniza.sk>
2024-06-19 22:12:44 +0200

    removed test variables

--- src/HernyBalik/Hra.java
+++ src/HernyBalik/Hra.java
@@ -14,11 +14,7 @@ public class Hra {
         ManazerEventov manazerEventov = new ManazerEventov(4800);
         new Mapa(hrac, manazerEventov);
         ObycajnyRytier r = new ObycajnyRytier("Justus", 5, 5);
-        ObycajnyRytier r2 = new ObycajnyRytier("Justus", 5, 5);
-        PokrocilyRytier r1 = new PokrocilyRytier("aBubu",Schopnost.DOBRODRUH, 2, 2);
         hrac.pridajRytiera(r);
-        hrac.pridajRytiera(r1);
-        hrac.pridajRytiera(r2);
     }
 
 }
